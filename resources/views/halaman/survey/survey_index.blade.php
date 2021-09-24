6a06acac34147a32946e7ec248547da60ad870e4
boven <boventvandeboo@gmail.com>
2021-09-24 15:33:19 +0700

    tes

--- resources/views/halaman/survey/survey_index.blade.php
+++ resources/views/halaman/survey/survey_index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="http://127.0.0.1:8000/css/survey.css">
+    <link href="{{ asset('/css/survey.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.15/tailwind.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <title>Support System</title>
